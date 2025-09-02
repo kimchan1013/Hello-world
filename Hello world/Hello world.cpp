7e93eb5ecab165ef89223b91d65d479caa91bed0
동찬 김 <kdc050477@naver.com>
2025-09-02 16:03:07 +0900

    2차 수정본

--- Hello world/Hello world.cpp
+++ Hello world/Hello world.cpp
@@ -1,77 +1,125 @@
-//#include <iostream>
+////#include <iostream>
+//
+//// int main()
+//// {
+//  //  std:cout << "Hello World!\n";
+//    //return 0;
+//// }
+////소스파일
+//// #pragma once
+////헤더파일
+////주석
+//// ctar+D :한줄 복제하기
+////shift+Deleate :한줄 삭제하기
+////shift+화cout << //이것을 한줄 출력하기
+////살표:블럭잡기
+////art+shift+화살표: 세로로 블럭 잡기
+//// //crrl+spcae : 자동완성가능 (안델리센스)창 띄우기
+////art+화살표위아래: 현재 줄을 위아래로 옮기기
+//// cart+B+B 프로젝트 빌드
+//// cart+B+R :프로젝트 다시 빌드하기 
+//// 프로그램 디버그: <F5>
+//// 
+///* 컴파일러 : 번역기(사람이 짜 놓은 코드를 기계가 알아듣게 번역해줌)
+//* -> 목적코드 생성
+//*  =>링커가 연결해서 실행파일 생성
+//* 
+//for (int i = 0; 1 (10, ++ 1)
+//빌드 : 실행파일 생성하기
+//리빌드 : */
+#include <iostream>
+#include <cstdio>
 
-// int main()
-// {
-  //  std:cout << "Hello World!\n";
-    //return 0;
-// }
-//소스파일
-// #pragma once
-//헤더파일
-//주석
-// ctar+D :한줄 복제하기
-//shift+Deleate :한줄 삭제하기
-//shift+화cout << //이것을 한줄 출력하기
-//살표:블럭잡기
-//art+shift+화살표: 세로로 블럭 잡기
-// //crrl+spcae : 자동완성가능 (안델리센스)창 띄우기
-//art+화살표위아래: 현재 줄을 위아래로 옮기기
-// cart+B+B 프로젝트 빌드
-// cart+B+R :프로젝트 다시 빌드하기 
-// 프로그램 디버그: <F5>
-// 
-/* 컴파일러 : 번역기(사람이 짜 놓은 코드를 기계가 알아듣게 번역해줌)
-* -> 목적코드 생성
-*  =>링커가 연결해서 실행파일 생성
-* 
-for (int i = 0; 1 (10, ++ 1)
-빌드 : 실행파일 생성하기
-리빌드 : */
+using namespace std;
+                       
+int main()  {
+int number1 = 0;
+int number2 = 0;
+printf("숫자 1을 입력하시오 : ");
+cin >> number1;
+printf("숫자 2를 입력하시오 : ");
+cin >> number2;
+int number3 = number1 + number2;
+printf("두 숫자의 합은 %d\n", number3);
 
-
-//#include<iostream>
-//int main() // 엔트리 포인트(코드가 시작되는 곳)
- // {
-    //괄호 시작과 끝맺음 필수!!
-
-//return 0; 
-
-//  }
-//std:cout << "Hello World!\n"; //c++ 출력방법
-//#include <iostream>  입출력 관련(cout)
-//int main()
-
-//#include "header.h"
-//std::printf("Hello World!"); //c언어 출력방법
-/*
-#include <stdio.h>
-
-int main()
-{
-    printf("Hello world\n");
-    printf("김동찬");
-    printf("06년생입니다.");
+return 0;
 }
-
-// 이스케이프 시퀀스
-// \n  : 줄바꾸기(개행문자)
-// \t: 탭 넣기 
-// \" :쌍따옴표 한개
-// \\:\찍기
-// \r :캐리지 리턴(윈도우에서는 중요하지 않음. 리눅스에서는 중요함)
-// 한글로 바꿔서 ㅁ (a)+오른쪽 컨트롤 특수기호 + 탭누르면 확장
-*/
-
-#include <stdio.h> 
-
-int numbe = 0;
-{
-    scanf("%d", &number);
-    printf("입력한 숫자는(c)) :%d\n", number);
-    std::cin >> number;
-    printf("입력한 숫자는(c++) : %d\n", number);
-
-    //아므ㅜ거나 
-
-    return 0;
-   }
+//
+// 
+// 
+// 
+// 
+////#include<iostream>
+////int main() // 엔트리 포인트(코드가 시작되는 곳)
+// // {
+//    //괄호 시작과 끝맺음 필수!!
+//
+////return 0; 
+//
+////  }
+////std:cout << "Hello World!\n"; //c++ 출력방법
+////#include <iostream>  입출력 관련(cout)
+////int main()
+//
+////#include "header.h"
+////std::printf("Hello World!"); //c언어 출력방법
+///*
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("Hello world\n");
+//    printf("김동찬");
+//    printf("06년생입니다.");
+//}
+//
+//// 이스케이프 시퀀스
+//// \n  : 줄바꾸기(개행문자)
+//// \t: 탭 넣기 
+//// \" :쌍따움표 한개
+//// \\:\찍기
+//// \r :캐리지 리턴(윈도우에서는 중요하지 않음. 리눅스에서는 중요함)
+//// 한글로 바꿔서 ㅁ (a)+오른쪽 컨트롤 특수기호 + 탭누르면 확장
+//*/
+///*변수(Variable)
+//-변하는 숫자
+//-컴퓨터에 값을 기억 시키기 위해 만들고 사용
+//*/
+//#include <stdio.h> 
+//// /<--나누기 뜻
+//
+//
+///*
+//연산자(Operator)
+//-계산을 하기 위한 기호
+//-대입 연산자
+//=
+//= 오른쪽에 있는 값을 왼쪽에 넣어라
+//a = b; // b에있는 값을 a에 넣어라
+//-산술 연산자
+//   사칙연산
+//    +(더하기) -(빼기) *(곱하기)  /(나누기) %(나머지 연산)
+//    a = 5%2;
+//    a에는 2가 들어간다.( 5를 3으로 나누었을 때 나머지가 2)
+//    -복합 대입연산자
+//     줄여쓰기용
+//     a += b;    //아래와 같은 코드
+//     a = a +b;
+//     -증감 연산자
+//      a++
+//*/
+//// == 양변이 같다.
+//int number = 0;  //number 라는 이름을 가진int(인티저)변수를 선언하고 거기에 0을 대입한다
+//int number; //변수선언.
+//number = 0; //변수에 값을 대입.
+////int : 정수형(소수점이 없는 숫자,5,10,-2,0,66 ...)
+//namber = 5.3;
+//number = 5 / 2; //number에는 2가 저장된다.
+//{
+//    scanf("%d", &number);
+//    printf("입력한 숫자는(c)) :%d\n", number);
+//    std::cin >> number;
+//    printf("입력한 숫자는(c++) : %d\n", number);
+//
+//    return 0;
+//   
